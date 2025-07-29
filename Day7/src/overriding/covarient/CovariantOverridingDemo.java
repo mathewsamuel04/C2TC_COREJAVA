4f4d55562cf6e30c28b7e9dde8bde2c38bc1a3da
monik <monik@DESKTOP-ITUTF8H>
2025-07-29 09:48:47 +0530

    Day7

---
+++ Day7/src/overriding/covarient/CovariantOverridingDemo.java
@@ -0,0 +1,16 @@
+package overriding.covarient;
+
+public class CovariantOverridingDemo {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+						Student student1 = new Student(1, "MCA", "Kapil");
+						student1.printData();
+
+						// Student object created using clone method
+						// assuming type casting is required
+						Student student2 = (Student) student1.clone();
+						student2.printData();
+		}
+		// TODO Auto-generated method stub
+
+}
